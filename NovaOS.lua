a1086a6918482f453f18b37a6159067f95cfc0ef
DigoLeGamer <dig076450@gmail.com>
2025-08-29 15:02:13 +0200

    Update NovaOS.lua

--- NovaOS.lua
+++ NovaOS.lua
@@ -5,7 +5,9 @@ if V1 == "" then
 	
 else
 	if V1 == "help" then
-		output("command not working", 1)
+		output("0/1 help [page_number]: for help", 1)
+	elseif V1 == "help 1" then
+		output("1/1 help this command isn't finished yet", 1)
 	else
 		output("command not found", 2)
 	end
